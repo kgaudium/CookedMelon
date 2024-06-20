ee1eb420e259d55acd31a679ab5feabb8d8ebedb
Gaudium <k.gaudium@gmail.com>
2024-06-20 18:07:40 +0300

    Added initial stuff

---
+++ src/main/java/kgaudium/friedmelon/FriedMelon.java
@@ -0,0 +1,13 @@
+package kgaudium.friedmelon;
+// TODO make an icon with fried melon slice
+import net.fabricmc.api.ModInitializer;
+
+public class FriedMelon implements ModInitializer{
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/kgaudium/friedmelon/FriedMelonDataGenerator.java
@@ -0,0 +1,19 @@
+package kgaudium.friedmelon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.data.DataProvider;
+
+public class FriedMelonDataGenerator implements DataGeneratorEntrypoint {
+    /**
+     * Register {@link DataProvider} with the {@link FabricDataGenerator} during this entrypoint.
+     *
+     * @param fabricDataGenerator The {@link FabricDataGenerator} instance
+     */
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        // Adding a provider example:
+        //
+        // pack.addProvider(AdvancementsProvider::new);
+    }
+}
